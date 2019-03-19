805d29c74a91214139c7aaacdb204e8c8f39fb49
Andy Poudret <andy.poudret@microej.com>
2019-03-19 17:47:48 +0100

    Create Android app project

---
+++ forgik-android/Application/src/main/java/com/choupom/forgik/android/MainActivity.java
@@ -0,0 +1,32 @@
+/*
+ * Java
+ *
+ * Copyright 2019 Andy Poudret. All rights reserved.
+ */
+package com.choupom.forgik.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+}
